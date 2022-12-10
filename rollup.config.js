41ccf7a49281b4564617ac73db99ac68bd6b6cd4
Kelvin Zhang <me@kelvinzhang.ca>
2022-12-10 13:59:57 -0500

    minify code

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,10 @@
+const { terser } = require("rollup-plugin-terser");
+
 module.exports = {
   input: "./index.js",
   output: {
     file: "./dist/content.js",
   },
   context: "window",
+  plugins: [terser()],
 };
